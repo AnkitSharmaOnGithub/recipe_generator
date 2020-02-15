76d9a7396c1acfa895c0b43041e7003c2ac5cc20
unknown <ankitsharma101299@gmail.com>
2020-02-15 19:59:28 +0530

    Added Js File.

---
+++ mealdb_assets/js/app.js
@@ -0,0 +1,29 @@
+var mealData;
+//DOM Elements
+// var DOMELements = {
+//     meal_name : 'meal_name',
+//     meal_recipe_image : 'meal_recipe--image',
+//     meal_details : 'meal_details',
+//     meal_recipe_text : 'meal_recipe--text',
+//     meal_ingredients : 'meal_ingredients',
+//     meal_video : 'meal_video',    
+// }
+
+// function generateDOMElements(DOMels){
+
+// }
+
+var meal_name = document.querySelector('.meal_name');
+var meal_recipe_image = document.querySelector('.meal_recipe--image');
+var meal_details = document.querySelector('.meal_details');
+var meal_recipe_text = document.querySelector('.meal_recipe--text');
+var meal_ingredients = document.querySelector('.meal_ingredients');
+var meal_video = document.querySelector('.meal_video');
+
+var normal__category = document.querySelector('.normal--category');
+var normal__area = document.querySelector('.normal--area');
+var normal__tags = document.querySelector('.normal--tags');
+var normal__drinks = document.querySelector('.normal--drinks');
+var ingredient__list = document.querySelector('.ingredient-list');
+var loader = document.querySelector('.lds-hourglass');
+
